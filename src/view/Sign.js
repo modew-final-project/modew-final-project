e562eb797e9954c8a1e6d2c638e0eb2300cb802a
하검 <87745742+ImperetilliDeus@users.noreply.github.com>
2023-04-03 15:32:16 +0900

    230403

--- src/view/Sign.js
+++ src/view/Sign.js
@@ -6,34 +6,61 @@ const Sign = ()=>{
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const [tel, setTel] = useState("");
 
     const onChange = (e) =>{
-        const{
-            target:{name,value},
-
-
-        } =e;
+        const {
+            target: { name, value },
+        } = e;
         if(name==="email"){
             setEmail(value);
-        }else if(name==="password"){
+        } else if(name==="password"){
             setPassword(value);
+        } else if(name==="name"){
+            setName(value);
+        } else if(name==="birthday"){
+            setBirthday(value);
+        } else if(name==="tel"){
+            setTel(value);
         }
     }
 
-    const onSubmit = async (e)=>{
 
+    const onSubmit = async (e) => {
         e.preventDefault();
-
+      
         try {
-            let data = await authService.signInWithEmailAndPassword(email,password);
-            console.log(data);
+          await fetch("http://localhost:3002/user", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              email: email,
+              pw: password,
+              name: name,
+              birthday: birthday,
+              tel: tel,
+              
+            }),
+          });
 
+          let data = await authService.createUserWithEmailAndPassword(email,password);
+            console.log("firebase에 전송된 데이터 : ",data);
+      
+          console.log("DB 저장 성공");
+      
+          // 회원가입 성공 메시지 클라이언트 쪽으로 전송
+          alert("회원가입이 완료되었습니다.");
         } catch (error) {
-            
-            console.log(error);
+          console.log("DB 저장 실패");
+          console.log(error);
+      
+          // 회원가입 실패 메시지 클라이언트 쪽으로 전송
+          alert("회원가입에 실패하였습니다.");
         }
-
-    }
+      };
+      
 
     return(
         <>
@@ -52,13 +79,13 @@ const Sign = ()=>{
                         <form>
                             <div className="user_input">
                                 <div className="loginBox">
-                                    <input type="text" onChange={onChange} placeholder="이름을 입력하세요." maxLength="50"/>
-                                    <input type="text" onChange={onChange} placeholder="주민등록번호를 입력하세요." maxLength="50"/>
-                                    <input type="text" onChange={onChange} placeholder="핸드폰번호를 입력하세요." maxLength="50"/>
-                                    <input type="text" onChange={onChange} placeholder="이메일을 입력하세요." maxLength="50"/>
+                                    <input type="text" onChange={onChange} name="name" placeholder="이름을 입력하세요." maxLength="50"/>
+                                    <input type="text" onChange={onChange} name="birthday" placeholder="주민등록번호를 입력하세요." maxLength="50"/>
+                                    <input type="text" onChange={onChange} name="tel" placeholder="핸드폰번호를 입력하세요." maxLength="50"/>
+                                    <input type="text" onChange={onChange} name="email" value={email} placeholder="이메일을 입력하세요." maxLength="50"/>
                                 </div>
                                 <div className="pwBox">
-                                    <input type="password" onChange={onChange} placeholder="비밀번호를 입력하세요."/>
+                                    <input type="password" onChange={onChange} value={password} name="password" placeholder="비밀번호를 입력하세요."/>
                                     <input type="password" onChange={onChange} placeholder="비밀번호를 확인해주세요"/>
                                 </div>
                             </div>
@@ -66,7 +93,7 @@ const Sign = ()=>{
                                 <input type="checkbox" id="saveId" value="false"/>
                                 <p>[필수] 이용약관 및 개인정보 처리방침에 동의합니다.</p>
                             </div> 
-                            <button className="submit">회원가입</button>
+                            <button className="submit" onClick={onSubmit}>회원가입</button>
                             <div className="login_bTxt">
                                 <ul>
                                     <li><a href="">로그인하기</a></li>
