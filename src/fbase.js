b743ab9e3c80cc75a88fdfa9ac2a2cb210230ffd
hyochang <yhc9308@naver.com>
2023-04-06 16:21:40 +0900

    파이어베이스
    ㅁ

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,8 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
